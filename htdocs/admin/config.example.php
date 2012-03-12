5013cdb76dbb52ce23e6d2d846fb20bea5873dc2
Thomas Munro <macdice@penski.net>
2012-03-12 21:56:01 +0000

    Created example config file, to keep real config out of source control

---
+++ htdocs/admin/config.example.php
@@ -0,0 +1,22 @@
+<?
+/** These variables need to be configured for each installation of zedquiz. */
+$DB_CONNECTION = "dbname=foo";
+$PROJECT_ROOT = "/path/to/root/of/zedquiz";
+$SMARTY = "/path/to/smarty/libs";
+
+/** Things below this line probably do not need to be changed. */
+require_once "$SMARTY/Smarty.class.php";
+
+function getSmarty() {
+    global $PROJECT_ROOT;
+    $smarty = new Smarty();;
+    $smarty->setTemplateDir("$PROJECT_ROOT/templates/");
+    $smarty->setCompileDir("$PROJECT_ROOT/templates_c/");
+    return $smarty;
+}
+
+function getDb() {
+    global $DB_CONNECTION;
+    return pg_pconnect($DB_CONNECTION);
+}
+?>
