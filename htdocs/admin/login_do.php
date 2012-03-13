3d4ad3d3e7b426175f3fc09489ac694004bdfecf
Thomas Munro <macdice@penski.net>
2012-03-13 01:03:21 +0000

    Show a message on auth fail

--- htdocs/admin/login_do.php
+++ htdocs/admin/login_do.php
@@ -8,7 +8,7 @@ $db = getDb();
 $result = pg_query_params($db, 'SELECT id, first_name, last_name FROM agent WHERE username = $1 AND password = crypt($2, password)', array($username, $password));
 $rows = pg_fetch_all($result);
 if (!$rows) {
-  header("location: login.php");
+  header("location: login.php?mode=retry");
 } else {
   $agent_id = $rows[0]["id"];
   pg_query_params($db, 'UPDATE agent SET last_seen = now() WHERE id = $1', array($agent_id));
